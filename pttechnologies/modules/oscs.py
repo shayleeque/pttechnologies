a88bd01abc2aed4ff65b93b3e0408af72bcc73d6
Daniel <d.kummel@penterep.com>
2025-06-15 16:51:04 +0200

    Add missing indent

--- pttechnologies/modules/oscs.py
+++ pttechnologies/modules/oscs.py
@@ -24,10 +24,9 @@ class OSCS:
         3) Report whether the server is case-insensitive (Windows) or case-sensitive (Unix/Linux).
         """
         ptprint(__TESTLABEL__, "TITLE", not self.args.json, colortext=True)
-        
         resource_url = self._find_static_resource()
         if not resource_url:
-            ptprint("No static source found for test", "ERROR", not self.args.json)
+            ptprint("No static source found for test", "ERROR", not self.args.json, indent=4)
             return
 
         lower_resp, ct_lower = self._fetch(resource_url)
@@ -36,7 +35,6 @@ class OSCS:
 
         self._report(lower_resp, ct_lower, upper_resp, ct_upper)
 
-    
     def _find_static_resource(self):
         """
         Locate a static resource to test:
@@ -54,7 +52,6 @@ class OSCS:
         resp, _ = self._fetch(favicon, allow_redirects=True)
         if resp.status_code == 200:
             return favicon
-        
         resp_home, _ = self._fetch(base + '/', allow_redirects=True)
         html = getattr(resp_home, 'text', '') or ''
         match = re.search(
@@ -73,7 +70,6 @@ class OSCS:
 
         path = parsed_cand.path if parsed_cand.path.startswith('/') else '/' + parsed_cand.path
         return parsed_base.scheme + '://' + parsed_base.netloc + path
-
 
     def _fetch(self, url, allow_redirects=True):
         """
@@ -111,7 +107,6 @@ class OSCS:
         upper = filename.upper()
         new_path = new_path = dirname + '/' + filename.upper()
         return urlunparse(parsed._replace(path=new_path))
-
 
     def _report(self, r1, ct1, r2, ct2):
         """

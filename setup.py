b0f0cb3b6e9b6937cbdb51eb75e57a501eb44394
Daniel <d.kummel@penterep.com>
2025-07-07 17:20:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ],
     python_requires='>=3.12',
-    install_requires=["ptlibs>=1.0.29,<2", ],
+    install_requires=["ptlibs>=1.0.30,<2", "bs4"],
     entry_points = {'console_scripts': ['pttechnologies = pttechnologies.pttechnologies:main']},
     include_package_data= True,
     long_description=long_description,
